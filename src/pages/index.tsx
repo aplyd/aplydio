f406d8da88590e3e54e1e5149fc116f16252d717
Austin Ftacnik <austin@ftacnik.com>
2023-07-09 14:53:16 -0700

    chore(index): remove boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,10 @@ import * as React from 'react';
 
 import Seo from '@/components/Seo';
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-
 export default function HomePage() {
   return (
     <>
-      {/* <Seo templateTitle='Home' /> */}
       <Seo />
-
       <main>
         <h1>Hello</h1>
       </main>
