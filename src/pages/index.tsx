c94a08759d437e98e1023a3b80376c38d3bad672
Austin Ftacnik <austin@ftacnik.com>
2023-07-14 18:17:21 -0700

    feat(index): add temporary content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 
+import ContentContainer from '@/components/layout/ContentContainer';
+import Hero from '@/components/layout/Hero';
 import Seo from '@/components/Seo';
 
 export default function HomePage() {
   return (
     <>
       <Seo />
-      <main>
-        <h1>Hello</h1>
+      <main className='min-h-[2000px] bg-white'>
+        <Hero />
+        <ContentContainer>
+          <h1>hello</h1>
+        </ContentContainer>
       </main>
     </>
   );
