80d6b7eef8274f1bd24286946fd9af5a606f885b
Austin Ftacnik <austin@ftacnik.com>
2023-07-16 19:10:52 -0700

    chore(scene): remove unused orbit controls

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,4 +1,3 @@
-import { OrbitControls } from '@react-three/drei';
 import { FC, PropsWithChildren } from 'react';
 
 import Lights from '@/components/canvas/Lights';
@@ -7,7 +6,6 @@ import Logo3DText from '@/components/canvas/Logo3DText';
 const Scene: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
-      <OrbitControls enabled={false} />
       <Lights />
       <Logo3DText />
       {children}
