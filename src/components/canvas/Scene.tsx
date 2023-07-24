c146f752aab114940920d28471a0834fa7e3087e
Austin Ftacnik <austin@ftacnik.com>
2023-07-23 18:28:08 -0700

    feat(scene): animate camera rotation

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -1,4 +1,6 @@
 import { OrbitControls } from '@react-three/drei';
+import { useFrame, useThree } from '@react-three/fiber';
+import { useSpring } from 'framer-motion';
 import { FC } from 'react';
 import { CubeTexture } from 'three';
 
@@ -12,13 +14,32 @@ interface SceneProps {
   cubeTexture: CubeTexture;
 }
 
+const ROTATION_STRENGTH = 0.5;
+
 const Scene: FC<SceneProps> = ({ transformedX, transformedY, cubeTexture }) => {
+  const { camera } = useThree();
+  const xSpring = useSpring(0);
+  const ySpring = useSpring(0);
+
+  useFrame(() => {
+    xSpring.set(transformedX);
+    ySpring.set(transformedY);
+
+    if (xSpring) {
+      camera.position.x = (xSpring.get() / 2) * ROTATION_STRENGTH;
+    }
+
+    if (ySpring) {
+      camera.position.y = (ySpring.get() / 2) * ROTATION_STRENGTH;
+    }
+  });
+
   return (
     <>
-      <OrbitControls />
       <Lights />
       <Environment cubeTexture={cubeTexture} />
       <Logo3DText transformedX={transformedX} transformedY={transformedY} />
+      <OrbitControls enableZoom={false} enablePan={false} />
     </>
   );
 };
